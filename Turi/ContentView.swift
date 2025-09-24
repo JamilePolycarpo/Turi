3d0ed1f6a6d658d2f5c85f2a53d08dbe68c53930
Jamile Polycarpo <j.polycarpo@gmail.com>
2025-09-24 14:01:28 -0300

    Initial Commit

---
+++ Turi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Turi
+//
+//  Created by Jamile Marian Polycarpo on 24/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Turi/TuriApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuriApp.swift
+//  Turi
+//
+//  Created by Jamile Marian Polycarpo on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TuriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
