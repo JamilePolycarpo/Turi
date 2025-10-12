044295cbf80936f1b067ad311853c4c4f0f87537
Andre Tonon <tonondeh87@gmail.com>
2025-10-12 14:10:30 -0300

    criacao da RegisterView

---
+++ Turi/View/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  Turi
+//
+//  Created by Andre Luiz Tonon on 12/10/25.
+//
+
+internal import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
