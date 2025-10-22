3b810d599c5b51897ada2b22ee1de0a2bb5c61fc
Andre Tonon <tonondeh87@gmail.com>
2025-10-22 10:52:32 -0300

    [update] implementação de botão com animação

--- Turi/Components/PasswordField.swift
+++ Turi/Components/PasswordField.swift
@@ -8,11 +8,44 @@
 internal import SwiftUI
 
 struct PasswordField: View {
+    let placeholder: String
+    @Binding var text: String
+    @State private var isSecure: Bool = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("ColorBackground")
+            VStack {
+                if isSecure {
+                    SecureField(placeholder, text: $text)
+                        .foregroundStyle(.white)
+                } else {
+                    TextField(placeholder, text: $text)
+                        .foregroundStyle(.white)
+                }
+                
+                Button(action: {
+                    withAnimation(.easeInOut(duration: 0.2)) {
+                        isSecure.toggle()
+                    }
+                }) {
+                    Image(systemName: isSecure ? "eye.slash" : "eye")
+                        .foregroundColor(.white.opacity(0.8))
+                }
+            }
+            .padding()
+            .background(Color.clear)
+            .overlay(
+                RoundedRectangle(cornerRadius: 40)
+                    .stroke(Color.white, lineWidth: 1)
+            )
+            .frame(height: 45)
+            .padding(.horizontal, 24)
+        }
     }
 }
 
 #Preview {
-    PasswordField()
+    PasswordField(placeholder: "Senha:", text: .constant(""))
+        .background(Color.black)
 }
