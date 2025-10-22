143607ad661385540770ad0b8c9af8be50524540
Andre Tonon <tonondeh87@gmail.com>
2025-10-22 10:32:11 -0300

    [feature] criação de component Password para reutilização

---
+++ Turi/Components/PasswordField.swift
@@ -0,0 +1,18 @@
+//
+//  PasswordField.swift
+//  Turi
+//
+//  Created by Andre Luiz Tonon on 22/10/25.
+//
+
+internal import SwiftUI
+
+struct PasswordField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PasswordField()
+}
